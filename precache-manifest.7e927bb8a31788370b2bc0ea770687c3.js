2fe746f7c459ca490c1184367aca21cffce1ce8f
hoangthai0302 <hnthai@humaxdigital.com>
2019-11-02 10:59:41 +0700

    Updates

---
+++ precache-manifest.7e927bb8a31788370b2bc0ea770687c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918b069d34722a79a7d2a428695fd556",
+    "url": "/test-ws/index.html"
+  },
+  {
+    "revision": "68830b12a1d087366114",
+    "url": "/test-ws/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69b6e87ff7cb8aab97a9",
+    "url": "/test-ws/static/js/2.7cb2dca6.chunk.js"
+  },
+  {
+    "revision": "68830b12a1d087366114",
+    "url": "/test-ws/static/js/main.c363b02b.chunk.js"
+  },
+  {
+    "revision": "360fd988739cc2d954fa",
+    "url": "/test-ws/static/js/runtime-main.23a3151b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-ws/static/media/logo.25bf045c.svg"
+  }
+]);
